f1d18655aac2f46383c8fa16feb6ba23fbb7af45
Deki-Grabs <dejangrabovica@proton.me>
2024-02-16 21:11:38 +0100

    Added slideshow on the landing page

--- script.js
+++ script.js
@@ -8,3 +8,24 @@ function openCloseMenu(){
         menu.style.display = 'none';
     }
 }
+
+const headerImg = document.querySelector('.landing-image')
+
+const UPDATE_TIME_INTERVAL = 3000
+
+const imagesArray = [
+  "Images/JA1.jpg",
+  "Images/panda.jpg",
+  "Images/marko.jpg",
+]
+
+let i = 0
+
+setInterval(()=>{
+  if(i == 2){
+    i = 0
+  }
+  else {i = i + 1};
+  console.log('i was run',i)
+  headerImg.src = imagesArray[i]
+},UPDATE_TIME_INTERVAL)
