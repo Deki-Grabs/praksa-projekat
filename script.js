ae8461cb7fa102b74c5e63ea7617a36b96a3251c
Deki-Grabs <dejangrabovica@proton.me>
2024-02-20 20:58:27 +0100

    And another one again

--- script.js
+++ script.js
@@ -9,23 +9,29 @@ function openCloseMenu(){
     }
 }
 
-// const headerImg = document.querySelector('.gallery-img')
+const headerImg = document.querySelector('.gallery-img')
 
-// const UPDATE_TIME_INTERVAL = 3000
+const UPDATE_TIME_INTERVAL = 3000
 
-// const imagesArray = [
-//   "Images/JA1.jpg",
-//   "Images/panda.jpg",
-//   "Images/marko.jpg",
-// ]
+const imagesArray = [
+  "Images/Gallery/gallery-1.jpg",
+  "Images/Gallery/gallery-2.jpg",
+  "Images/Gallery/gallery-3.jpg",
+  "Images/Gallery/gallery-4.jpg",
+  "Images/Gallery/gallery-5.jpg",
+  "Images/Gallery/gallery-6.jpg",
+  "Images/Gallery/gallery-7.jpg",
+  "Images/Gallery/gallery-8.jpg",
+  "Images/Gallery/gallery-9.jpg",
+]
 
-// let i = 0
+let i = 0
 
-// setInterval(()=>{
-//   if(i == 2){
-//     i = 0
-//   }
-//   else {i = i + 1};
-//   console.log('i was run',i)
-//   headerImg.src = imagesArray[i]
-// },UPDATE_TIME_INTERVAL)
+setInterval(()=>{
+  if(i == 2){
+    i = 0
+  }
+  else {i = i + 1};
+  console.log('i was run',i)
+  headerImg.src = imagesArray[i]
+},UPDATE_TIME_INTERVAL)
